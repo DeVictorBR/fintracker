e9d67bcc2c0de7e0e86ad435be1c4b6013ab5b16
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-23 05:26:00 -0300

    feat(controller): add AuthController with signup endpoint

---
+++ src/main/java/com/victorbarbosa/fintracker/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.victorbarbosa.fintracker.controller;
+
+import com.victorbarbosa.fintracker.controller.dto.SignupCreateRequest;
+import com.victorbarbosa.fintracker.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody @Valid SignupCreateRequest req) {
+        authService.signup(req);
+        return ResponseEntity.ok("User created successfully!");
+    }
+}
