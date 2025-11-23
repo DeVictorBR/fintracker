9c11fc663bc5ecf106cb8a36cae0f755fd0362eb
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-23 04:40:11 -0300

    feat(repository): add UserRepository for DB access

---
+++ src/main/java/com/victorbarbosa/fintracker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.victorbarbosa.fintracker.repository;
+
+import com.victorbarbosa.fintracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
