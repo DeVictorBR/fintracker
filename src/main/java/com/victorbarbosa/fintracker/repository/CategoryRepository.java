6e4665baaaa8a7f1bd272cd690ce751899bac440
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-23 16:52:18 -0300

    feat: add CategoryRepository with user filtering

---
+++ src/main/java/com/victorbarbosa/fintracker/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.victorbarbosa.fintracker.repository;
+
+import com.victorbarbosa.fintracker.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Page<Category> findByUserId(Long id, Pageable pageable);
+}
