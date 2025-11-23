cb54210cfd61bea31b2783e19a1fed2816d07a04
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-23 04:20:37 -0300

    feat(entity): add Role entity

---
+++ src/main/java/com/victorbarbosa/fintracker/entity/Role.java
@@ -0,0 +1,58 @@
+package com.victorbarbosa.fintracker.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_role_authority",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private Set<Authority> authorities = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(String name, Set<Authority> authorities) {
+        this.name = name;
+        this.authorities = authorities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
+}
