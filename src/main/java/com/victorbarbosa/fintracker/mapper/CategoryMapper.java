5e93c656813cf1b04ab5c6a43ee539f30f13e89d
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-23 21:26:24 -0300

    feat(mapper): add toUp method in CategoryMapper for update mapping

--- src/main/java/com/victorbarbosa/fintracker/mapper/CategoryMapper.java
+++ src/main/java/com/victorbarbosa/fintracker/mapper/CategoryMapper.java
@@ -2,6 +2,7 @@ package com.victorbarbosa.fintracker.mapper;
 
 import com.victorbarbosa.fintracker.controller.dto.CategoryCreateRequest;
 import com.victorbarbosa.fintracker.controller.dto.CategoryCreateResponse;
+import com.victorbarbosa.fintracker.controller.dto.CategoryUpdateResponse;
 import com.victorbarbosa.fintracker.entity.Category;
 import com.victorbarbosa.fintracker.entity.User;
 
@@ -16,8 +17,17 @@ public class CategoryMapper {
         );
     }
 
-    public static CategoryCreateResponse toResponse(Category category) {
+    public static CategoryCreateResponse to(Category category) {
         return new CategoryCreateResponse(
+                category.getId(),
+                category.getName(),
+                category.getDescription(),
+                category.getType()
+        );
+    }
+
+    public static CategoryUpdateResponse toUp(Category category) {
+        return new CategoryUpdateResponse(
                 category.getId(),
                 category.getName(),
                 category.getDescription(),
