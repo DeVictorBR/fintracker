fff2961308b6675bf981aa8704547feb2e28e785
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-23 21:27:37 -0300

    feat(service): add getCategoryByIdAndUser and update methods in CategoryService

--- src/main/java/com/victorbarbosa/fintracker/service/CategoryService.java
+++ src/main/java/com/victorbarbosa/fintracker/service/CategoryService.java
@@ -3,6 +3,9 @@ package com.victorbarbosa.fintracker.service;
 import com.victorbarbosa.fintracker.base.PageResponse;
 import com.victorbarbosa.fintracker.controller.dto.CategoryCreateRequest;
 import com.victorbarbosa.fintracker.controller.dto.CategoryCreateResponse;
+import com.victorbarbosa.fintracker.controller.dto.CategoryUpdateRequest;
+import com.victorbarbosa.fintracker.controller.dto.CategoryUpdateResponse;
+import com.victorbarbosa.fintracker.entity.Category;
 import com.victorbarbosa.fintracker.entity.User;
 import com.victorbarbosa.fintracker.exception.CategoryNotFoundException;
 import com.victorbarbosa.fintracker.mapper.CategoryMapper;
@@ -28,29 +31,42 @@ public class CategoryService {
         var user = getAuthenticatedUser(auth);
         var category = CategoryMapper.from(req, user);
         var savedCategory = categoryRepository.save(category);
-        return CategoryMapper.toResponse(savedCategory);
+        return CategoryMapper.to(savedCategory);
     }
 
     public PageResponse<CategoryCreateResponse> findAll(Pageable pageable, Authentication auth) {
         var user = getAuthenticatedUser(auth);
         var page = categoryRepository.findByUserId(user.getId(), pageable);
-        var pageResp = page.map(CategoryMapper::toResponse);
+        var pageResp = page.map(CategoryMapper::to);
         return PageMapper.toPageResponse(pageResp);
     }
 
     public CategoryCreateResponse findById(Long id, Authentication auth) {
         var user = getAuthenticatedUser(auth);
+        var category = getCategoryByIdAndUser(id, user);
+        return CategoryMapper.to(category);
+    }
+
+    public CategoryUpdateResponse update(CategoryUpdateRequest req, Long id, Authentication auth) {
+        var user = getAuthenticatedUser(auth);
+        var category = getCategoryByIdAndUser(id, user);
+        category.setName(req.name());
+        category.setDescription(req.description());
+        var savedCaegory = categoryRepository.save(category);
+        return CategoryMapper.toUp(savedCaegory);
+    }
+
+    private User getAuthenticatedUser(Authentication auth) {
+        return userRepository.findByEmail(auth.getName()).orElseThrow();
+    }
+
+    private Category getCategoryByIdAndUser(Long id, User user) {
         var category = categoryRepository.findById(id)
                 .orElseThrow(() -> new CategoryNotFoundException("Category with id: " + id + " not found"));
 
         if (!category.getUser().getId().equals(user.getId())) {
             throw new CategoryNotFoundException("Category with id: " + id + " not found");
         }
-
-        return CategoryMapper.toResponse(category);
-    }
-
-    private User getAuthenticatedUser(Authentication auth) {
-        return userRepository.findByEmail(auth.getName()).orElseThrow();
+        return category;
     }
 }
