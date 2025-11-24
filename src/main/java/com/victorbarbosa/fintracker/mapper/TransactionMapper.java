1b3c13aad073eb0f58f2cb8dcf3c4ae66d9fb167
Victor Barbosa <victorhenriquebl11@gmail.com>
2025-11-24 15:15:19 -0300

    feat(mapper): add TransactionMapper for casting

---
+++ src/main/java/com/victorbarbosa/fintracker/mapper/TransactionMapper.java
@@ -0,0 +1,32 @@
+package com.victorbarbosa.fintracker.mapper;
+
+import com.victorbarbosa.fintracker.controller.dto.TransactionCreateRequest;
+import com.victorbarbosa.fintracker.controller.dto.TransactionCreateResponse;
+import com.victorbarbosa.fintracker.entity.Category;
+import com.victorbarbosa.fintracker.entity.Transaction;
+import com.victorbarbosa.fintracker.entity.User;
+
+public class TransactionMapper {
+
+    public static Transaction from(TransactionCreateRequest req, User user, Category category) {
+        return new Transaction(
+                req.description(),
+                req.amount(),
+                req.type(),
+                req.date(),
+                category,
+                user
+        );
+    }
+
+    public static TransactionCreateResponse to(Transaction transaction) {
+        return new TransactionCreateResponse(
+                transaction.getId(),
+                transaction.getDescription(),
+                transaction.getAmount(),
+                transaction.getType(),
+                transaction.getDate(),
+                transaction.getCategory().getId()
+        );
+    }
+}
